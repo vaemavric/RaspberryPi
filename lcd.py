2e8ad6414558788fdede5c9bacb814f6bda3d8de
vaemavric <vaemavric@gmail.com>
2016-09-05 12:42:45 +0100

    commit 4

--- lcd.py
+++ lcd.py
@@ -15,7 +15,7 @@
 # http://www.raspberrypi-spy.co.uk/
 #
 #--------------------------------------
-
+ 
 # The wiring for the LCD is as follows:
 # 1 : GND
 # 2 : 5V
@@ -33,11 +33,11 @@
 # 14: Data Bit 7
 # 15: LCD Backlight +5V**
 # 16: LCD Backlight GND
-
+ 
 #import
 import RPi.GPIO as GPIO
 import time
-
+ 
 # Define GPIO to LCD mapping
 LCD_RS = 7
 LCD_E  = 8
@@ -45,22 +45,59 @@ LCD_D4 = 25
 LCD_D5 = 24
 LCD_D6 = 23
 LCD_D7 = 18
-
-
+ 
 # Define some device constants
 LCD_WIDTH = 16    # Maximum characters per line
 LCD_CHR = True
 LCD_CMD = False
-
+ 
 LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
 LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
+ 
 # Timing constants
 E_PULSE = 0.0005
 E_DELAY = 0.0005
-
-
-
+ 
+def main():
+  # Main program block
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+ 
+  # Initialise display
+  lcd_init()
+ 
+  while True:
+ 
+    # Send some test
+    lcd_string("Rasbperry Pi",LCD_LINE_1)
+    lcd_string("16x2 LCD Test",LCD_LINE_2)
+ 
+    time.sleep(3) # 3 second delay
+ 
+    # Send some text
+    lcd_string("1234567890123456",LCD_LINE_1)
+    lcd_string("abcdefghijklmnop",LCD_LINE_2)
+ 
+    time.sleep(3) # 3 second delay
+ 
+    # Send some text
+    lcd_string("RaspberryPi-spy",LCD_LINE_1)
+    lcd_string(".co.uk",LCD_LINE_2)
+ 
+    time.sleep(3)
+ 
+    # Send some text
+    lcd_string("Follow me on",LCD_LINE_1)
+    lcd_string("Twitter @RPiSpy",LCD_LINE_2)
+ 
+    time.sleep(3)
+ 
 def lcd_init():
   # Initialise display
   lcd_byte(0x33,LCD_CMD) # 110011 Initialise
@@ -70,15 +107,15 @@ def lcd_init():
   lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
   lcd_byte(0x01,LCD_CMD) # 000001 Clear display
   time.sleep(E_DELAY)
-
+ 
 def lcd_byte(bits, mode):
   # Send byte to data pins
   # bits = data
   # mode = True  for character
   #        False for command
-
+ 
   GPIO.output(LCD_RS, mode) # RS
-
+ 
   # High bits
   GPIO.output(LCD_D4, False)
   GPIO.output(LCD_D5, False)
@@ -92,10 +129,10 @@ def lcd_byte(bits, mode):
     GPIO.output(LCD_D6, True)
   if bits&0x80==0x80:
     GPIO.output(LCD_D7, True)
-
+ 
   # Toggle 'Enable' pin
   lcd_toggle_enable()
-
+ 
   # Low bits
   GPIO.output(LCD_D4, False)
   GPIO.output(LCD_D5, False)
@@ -109,10 +146,10 @@ def lcd_byte(bits, mode):
     GPIO.output(LCD_D6, True)
   if bits&0x08==0x08:
     GPIO.output(LCD_D7, True)
-
+ 
   # Toggle 'Enable' pin
   lcd_toggle_enable()
-
+ 
 def lcd_toggle_enable():
   # Toggle enable
   time.sleep(E_DELAY)
@@ -120,70 +157,24 @@ def lcd_toggle_enable():
   time.sleep(E_PULSE)
   GPIO.output(LCD_E, False)
   time.sleep(E_DELAY)
-
+ 
 def lcd_string(message,line):
   # Send string to display
-
-
-
-
+ 
   message = message.ljust(LCD_WIDTH," ")
-
+ 
   lcd_byte(line, LCD_CMD)
-
+ 
   for i in range(LCD_WIDTH):
     lcd_byte(ord(message[i]),LCD_CHR)
-	
-def main():
-  # Main program block
-
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-
-
-  # Initialise display
-  lcd_init()
-
-  while True:
-
-    # Send some test
-    lcd_string("Rasbperry Pi",LCD_LINE_1)
-    lcd_string("16x2 LCD Test",LCD_LINE_2)
-
-    time.sleep(3) # 3 second delay
-
-    # Send some text
-    lcd_string("1234567890123456",LCD_LINE_1)
-    lcd_string("abcdefghijklmnop",LCD_LINE_2)
-
-
-
-    time.sleep(3) # 3 second delay
-
-    # Send some text
-    lcd_string("RaspberryPi-spy",LCD_LINE_1)
-    lcd_string(".co.uk",LCD_LINE_2)
-
-    time.sleep(3)
-
-    # Send some text
-    lcd_string("Follow me on",LCD_LINE_1)
-    lcd_string("Twitter @RPiSpy",LCD_LINE_2)
-
-    time.sleep(3)
-
+ 
 if __name__ == '__main__':
-
+ 
   try:
     main()
   except KeyboardInterrupt:
     pass
   finally:
-    lcd_byte(0x01, False)
+    lcd_byte(0x01, LCD_CMD)
     lcd_string("Goodbye!",LCD_LINE_1)
     GPIO.cleanup()
