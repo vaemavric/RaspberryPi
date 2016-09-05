ba72eae4b3cd717158cf83d9e7dbbf48541e5fc4
vaemavric <vaemavric@gmail.com>
2016-09-05 12:57:59 +0100

    commit 8

--- lcd.py
+++ lcd.py
@@ -38,28 +38,9 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
 def main():
-
-# Define GPIO to LCD mapping
-  LCD_RS = 7
-  LCD_E  = 8
-  LCD_D4 = 25
-  LCD_D5 = 24
-  LCD_D6 = 23
-  LCD_D7 = 18
-
-
-# Define some device constants
-  LCD_WIDTH = 16    # Maximum characters per line
-  LCD_CHR = True
-  LCD_CMD = False
-
-  LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-  LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
-# Timing constants
-  E_PULSE = 0.0005
-  E_DELAY = 0.0005
   # Main program block
 
   GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
@@ -176,7 +157,26 @@ def lcd_string(message,line):
     lcd_byte(ord(message[i]),LCD_CHR)
 
 if __name__ == '__main__':
+# Define GPIO to LCD mapping
+  LCD_RS = 7
+  LCD_E  = 8
+  LCD_D4 = 25
+  LCD_D5 = 24
+  LCD_D6 = 23
+  LCD_D7 = 18
 
+
+# Define some device constants
+  LCD_WIDTH = 16    # Maximum characters per line
+  LCD_CHR = True
+  LCD_CMD = False
+
+  LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+  LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+# Timing constants
+  E_PULSE = 0.0005
+  E_DELAY = 0.0005
   try:
     main()
   except KeyboardInterrupt:
