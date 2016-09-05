e5ca9732a5915067a60a0f5ee86221250250275d
vaemavric <vaemavric@gmail.com>
2016-09-05 12:55:35 +0100

    commit 6

--- lcd.py
+++ lcd.py
@@ -38,27 +38,6 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO to LCD mapping
-LCD_RS = 7
-LCD_E  = 8
-LCD_D4 = 25
-LCD_D5 = 24
-LCD_D6 = 23
-LCD_D7 = 18
-
-
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
-
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
-
 def main():
 
 # Define GPIO to LCD mapping
