9965802c31d35930adb10cc15c828fbd734ce5bc
vaemavric <vaemavric@gmail.com>
2016-09-05 12:45:16 +0100

    commit 4

--- lcd.py
+++ lcd.py
@@ -63,7 +63,7 @@ def main():
   GPIO.setwarnings(False)
   GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
   GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(7, GPIO.OUT) # RS
   GPIO.setup(LCD_D4, GPIO.OUT) # DB4
   GPIO.setup(LCD_D5, GPIO.OUT) # DB5
   GPIO.setup(LCD_D6, GPIO.OUT) # DB6
